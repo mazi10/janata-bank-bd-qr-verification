17438023c12422b3d8971fa6614848ed22aae8bd
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-20 16:31:20 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -2,7 +2,15 @@ const bankData = {
     // admin.html থেকে জেনারেট করা কোড এখানে পেস্ট করবেন
         
 
-    
+        "1231": {
+        headerHtml: `<p><strong style="color: rgb(0, 102, 204);"><em>JANATA BANK P.L.C.</em></strong></p>`,
+        fontFamily: "'Poppins', sans-serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        date: "18-Sep-2026",
+        statusText: "Verification success!!!"
+    },
 
 
 
